ac6daec32cccd487e0bc6f126de42aebf6c711a8
Milene <milenesantosalmeida13@gmail.com>
2023-11-10 16:40:21 -0300

    Desafio criado durante a aula feito sem erros

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # TDD-Dio-Testes-Unitarios-NET-Core
-Desafio de programação em .NET proposto no módulo - Blindando Seu Código com TDD e Testes Unitários Usando .NET Core da DIO
+Desafio de programação em .NET proposto e implementado em aula no módulo - Blindando Seu Código com TDD e Testes Unitários Usando .NET Core da DIO. Apresentado por Mauro Junior.
---
+++ newTalents/Calculadora.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace newTalents
+{
+    public class Calculadora
+    {
+        private List<string> _historico;
+        private string data;
+        public Calculadora(string data)
+        {
+            _historico = new List<string>();
+            this.data = data;
+        }
+
+        public int Somar(int val1, int val2)
+        {
+            int res = val1 + val2;
+            _historico.Insert(0, "Resultado: " + res + " - Data: " + data);
+            return res;
+        }
+
+        public int Subtrair(int val1, int val2)
+        {
+            int res = val1 - val2;
+            _historico.Insert(0, "Resultado: " + res + " - Data: " + data);
+            return res;
+        }
+
+        public int Dividir(int val1, int val2)
+        {
+            int res = val1 / val2;
+            _historico.Insert(0, "Resultado: " + res + " - Data: " + data);
+            return res;
+        }
+
+        public int Multiplicar(int val1, int val2)
+        {
+            int res = val1 * val2;
+            _historico.Insert(0, "Resultado: " + res + " - Data: " + data);
+            return res;
+        }
+
+        public List<string> historico()
+        {
+            _historico.RemoveRange(3, _historico.Count - 3);
+            return _historico;
+        }
+    }
+}
